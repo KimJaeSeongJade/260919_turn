75eceb1244c391e8f94be89f99436bcb7eeea57e
Jade <jade16work@gmail.com>
2026-09-19 14:14:56 +0900

    [Feat] 배틀씬 유닛 스폰 구현

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoints;
+    
+    private List<Unit> _playerUnits = new List<Unit>();
+    private List<Unit> _monsterUnits = new List<Unit>();
+    
+    private void Start() => Init();
+    
+    
+    
+    private void Init()
+    {
+        foreach (Unit unit in GameManager.Instance.PlayerParty)
+        {
+            _playerUnits.Add(Instantiate(unit));
+        }
+
+        foreach (Unit unit in GameManager.Instance.MonsterParty)
+        {
+            _monsterUnits.Add(Instantiate(unit));
+        }
+
+        for (int i = 0; i < _playerUnits.Count; i++)
+        {
+            // 0, 1, 2
+            _playerUnits[i].transform.position = _spawnPoints[i].position;
+        }
+
+        for (int i = 0; i < _monsterUnits.Count; i++)
+        {
+            // 3, 4, 5
+            _monsterUnits[i].transform.position = _spawnPoints[i + 3].position;
+        }
+    }
+}
